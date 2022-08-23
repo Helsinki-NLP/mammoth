b0d699ede0df59fb199ed3e4daa9db812f3611d7
Stig-Arne Grönroos <stig.gronroos@gmail.com>
2022-08-23 17:28:25 +0300

    Fix --scheduling_strategy default

--- onmt/opts.py
+++ onmt/opts.py
@@ -835,7 +835,13 @@ def _add_train_general_opts(parser):
         default=[0],
         help="Steps at which accum_count values change",
     )
-    group.add('--scheduling_strategy', '-scheduling_strategy', choices=SCHEDULING_STRATEGIES.keys(), default='sampling')
+    group.add(
+        '--scheduling_strategy',
+        '-scheduling_strategy',
+        choices=SCHEDULING_STRATEGIES.keys(),
+        default='weighted_sampling',
+        help="Strategy for the order in which tasks (e.g. language pairs) are scheduled for training"
+    )
     group.add('--valid_steps', '-valid_steps', type=int, default=10000, help='Perfom validation every X steps')
     group.add('--valid_batch_size', '-valid_batch_size', type=int, default=32, help='Maximum batch size for validation')
     group.add(

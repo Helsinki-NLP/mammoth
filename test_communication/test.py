010b32a912f0e5c9c8771a4b9070ef1ca40176d6
Michele Boggia <michele.boggia@getjenny.com>
2022-08-25 16:18:45 +0300

    increase timeout limits and comment out translation test (broken)

--- test_communication/test.py
+++ test_communication/test.py
@@ -28,7 +28,7 @@ class TestTraining(TestCase):
         components_gen = ["{}_gen".format(tgt_lang) for tgt_lang in ast.literal_eval(opt.tgt_vocab).keys()]
         return ["frame", "bridge", *components_enc, *components_dec, *components_gen]
 
-    @timeout_decorator.timeout(30)
+    @timeout_decorator.timeout(60)
     def test_training_1gpu_4pairs(self):
         out_model_prefix = "wmt_1gpu_4pairs"
         opt, _ = self.parser.parse_known_args(
@@ -64,7 +64,7 @@ class TestTraining(TestCase):
                 logger.info("Removing file {}".format(out_file))
                 os.remove(out_file)
 
-    @timeout_decorator.timeout(30)
+    @timeout_decorator.timeout(60)
     def test_training_1gpu_4pairs_ab_lin(self):
         out_model_prefix = "wmt_1gpu_4pairs_lin"
         opt, _ = self.parser.parse_known_args(
@@ -106,7 +106,7 @@ class TestTraining(TestCase):
                 logger.info("Removing file {}".format(out_file))
                 os.remove(out_file)
 
-    @timeout_decorator.timeout(30)
+    @timeout_decorator.timeout(60)
     def test_training_1gpu_4pairs_ab_ff(self):
         out_model_prefix = "wmt_1gpu_4pairs_ff"
         opt, _ = self.parser.parse_known_args(
@@ -146,7 +146,7 @@ class TestTraining(TestCase):
                 logger.info("Removing file {}".format(out_file))
                 os.remove(out_file)
 
-    @timeout_decorator.timeout(30)
+    @timeout_decorator.timeout(60)
     def test_training_1gpu_4pairs_ab_tf(self):
         out_model_prefix = "wmt_1gpu_4pairs_tf"
         opt, _ = self.parser.parse_known_args(
@@ -186,7 +186,7 @@ class TestTraining(TestCase):
                 logger.info("Removing file {}".format(out_file))
                 os.remove(out_file)
 
-    @timeout_decorator.timeout(30)
+    @timeout_decorator.timeout(60)
     def test_training_1gpu_4pairs_ab_simple(self):
         out_model_prefix = "wmt_1gpu_4pairs_simple"
         opt, _ = self.parser.parse_known_args(
@@ -226,7 +226,7 @@ class TestTraining(TestCase):
                 logger.info("Removing file {}".format(out_file))
                 os.remove(out_file)
 
-    @timeout_decorator.timeout(30)
+    @timeout_decorator.timeout(60)
     def test_training_1gpu_4pairs_ab_perceiver(self):
         out_model_prefix = "wmt_1gpu_4pairs_perceiver"
         opt, _ = self.parser.parse_known_args(
@@ -266,7 +266,7 @@ class TestTraining(TestCase):
                 logger.info("Removing file {}".format(out_file))
                 os.remove(out_file)
 
-    @timeout_decorator.timeout(40)
+    @timeout_decorator.timeout(60)
     def test_training_2gpus_4pairs(self):
         out_model_prefix = "wmt_2gpus_4pairs"
         opt, _ = self.parser.parse_known_args(
@@ -303,7 +303,7 @@ class TestTraining(TestCase):
                 logger.info("Removing file {}".format(out_file))
                 os.remove(out_file)
 
-    @timeout_decorator.timeout(40)
+    @timeout_decorator.timeout(60)
     def test_training_2gpus_4pairs_ab_lin(self):
         out_model_prefix = "wmt_2gpus_4pairs_lin"
         opt, _ = self.parser.parse_known_args(
@@ -346,7 +346,7 @@ class TestTraining(TestCase):
                 logger.info("Removing file {}".format(out_file))
                 os.remove(out_file)
 
-    @timeout_decorator.timeout(40)
+    @timeout_decorator.timeout(60)
     def test_training_2gpus_4pairs_ab_ff(self):
         out_model_prefix = "wmt_2gpus_4pairs_ff"
         opt, _ = self.parser.parse_known_args(
@@ -387,7 +387,7 @@ class TestTraining(TestCase):
                 logger.info("Removing file {}".format(out_file))
                 os.remove(out_file)
 
-    @timeout_decorator.timeout(40)
+    @timeout_decorator.timeout(60)
     def test_training_2gpus_4pairs_ab_tf(self):
         out_model_prefix = "wmt_2gpus_4pairs_tf"
         opt, _ = self.parser.parse_known_args(
@@ -428,7 +428,7 @@ class TestTraining(TestCase):
                 logger.info("Removing file {}".format(out_file))
                 os.remove(out_file)
 
-    @timeout_decorator.timeout(40)
+    @timeout_decorator.timeout(60)
     def test_training_2gpus_4pairs_ab_simple(self):
         out_model_prefix = "wmt_2gpus_4pairs_simple"
         opt, _ = self.parser.parse_known_args(
@@ -471,7 +471,7 @@ class TestTraining(TestCase):
                 logger.info("Removing file {}".format(out_file))
                 os.remove(out_file)
 
-    @timeout_decorator.timeout(40)
+    @timeout_decorator.timeout(60)
     def test_training_2gpus_4pairs_ab_perceiver(self):
         out_model_prefix = "wmt_2gpus_4pairs_perceiver"
         opt, _ = self.parser.parse_known_args(
@@ -512,7 +512,7 @@ class TestTraining(TestCase):
                 logger.info("Removing file {}".format(out_file))
                 os.remove(out_file)
 
-    @timeout_decorator.timeout(40)
+    @timeout_decorator.timeout(60)
     def test_training_2gpus_4pairs_crossed(self):
         out_model_prefix = "wmt_2gpus_4pairs_crossed"
         opt, _ = self.parser.parse_known_args(
@@ -549,7 +549,7 @@ class TestTraining(TestCase):
                 logger.info("Removing file {}".format(out_file))
                 os.remove(out_file)
 
-    @timeout_decorator.timeout(40)
+    @timeout_decorator.timeout(60)
     def test_training_4gpus_4pairs(self):
         out_model_prefix = "wmt_4gpus_4pairs"
         opt, _ = self.parser.parse_known_args(
@@ -588,7 +588,7 @@ class TestTraining(TestCase):
                 logger.info("Removing file {}".format(out_file))
                 os.remove(out_file)
 
-    @timeout_decorator.timeout(60)
+    @timeout_decorator.timeout(120)
     def test_training_3gpus_12pairs(self):
         out_model_prefix = "wmt_3gpus_12pairs"
         opt, _ = self.parser.parse_known_args(
@@ -634,7 +634,7 @@ class TestTraining(TestCase):
                 logger.info("Removing file {}".format(out_file))
                 os.remove(out_file)
 
-    @timeout_decorator.timeout(80)
+    @timeout_decorator.timeout(120)
     def test_training_3gpus_21pairs(self):
         out_model_prefix = "wmt_3gpus_21pairs"
         opt, _ = self.parser.parse_known_args(
@@ -689,7 +689,7 @@ class TestTraining(TestCase):
                 logger.info("Removing file {}".format(out_file))
                 os.remove(out_file)
 
-    @timeout_decorator.timeout(60)
+    @timeout_decorator.timeout(120)
     def test_training_4gpus_12pairs(self):
         out_model_prefix = "wmt_4gpus_12pairs"
         opt, _ = self.parser.parse_known_args(
@@ -736,7 +736,7 @@ class TestTraining(TestCase):
                 logger.info("Removing file {}".format(out_file))
                 os.remove(out_file)
 
-    @timeout_decorator.timeout(100)
+    @timeout_decorator.timeout(120)
     def test_training_4gpus_24pairs(self):
         out_model_prefix = "wmt_4gpus_24pairs"
         opt, _ = self.parser.parse_known_args(
@@ -795,7 +795,7 @@ class TestTraining(TestCase):
                 logger.info("Removing file {}".format(out_file))
                 os.remove(out_file)
 
-    @timeout_decorator.timeout(50)
+    @timeout_decorator.timeout(120)
     def test_training_1gpu_tensorboard(self):
         out_model_prefix = "wmt_1gpu_tb"
         opt, _ = self.parser.parse_known_args(
@@ -841,7 +841,7 @@ class TestTraining(TestCase):
             logger.info("Removing folder {}".format("tensorboard/{}".format(out_model_prefix)))
             shutil.rmtree("tensorboard/{}".format(out_model_prefix))
 
-    @timeout_decorator.timeout(50)
+    @timeout_decorator.timeout(120)
     def test_training_2gpus_tensorboard(self):
         out_model_prefix = "wmt_2gpus_tb"
         opt, _ = self.parser.parse_known_args(
@@ -891,34 +891,34 @@ class TestTraining(TestCase):
             shutil.rmtree("tensorboard/{}".format(out_model_prefix))
 
 
-class TestTranslate(TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        cls.parser = ArgumentParser(description="translate.py")
-        onmt.opts.config_opts(cls.parser)
-        onmt.opts.translate_opts(cls.parser)
-        onmt.opts.build_bilingual_model(cls.parser)
-
-    def test_translate(self):
-        # TODO: train model instead of loading one the one used now,
-        # remove all absolute paths, add test data in the repo
-        opt, _ = self.parser.parse_known_args(
-            [
-                "-gpu",
-                "0",
-                "-data_type",
-                "text",
-                "-src_lang",
-                "en",
-                "-tgt_lang",
-                "fr",
-                "-model",
-                "/home/micheleb/models/scaleUpMNMT/opus12/opus12.50.adaf_step_3000_",
-                "-src",
-                "/home/micheleb/data/scaleUpMNMT/prepare_opus_data_out/supervised/en-fr/opus.en-fr-dev.en.sp",
-                "-output",
-                "/home/micheleb/projects/OpenNMT-py-v2/translate_test.tmp",
-                "-use_attention_bridge",
-            ]
-        )
-        translate(opt)
+# class TestTranslate(TestCase):
+#     @classmethod
+#     def setUpClass(cls) -> None:
+#         cls.parser = ArgumentParser(description="translate.py")
+#         onmt.opts.config_opts(cls.parser)
+#         onmt.opts.translate_opts(cls.parser)
+#         onmt.opts.build_bilingual_model(cls.parser)
+#
+#     def test_translate(self):
+#         # TODO: train model instead of loading one the one used now,
+#         # remove all absolute paths, add test data in the repo
+#         opt, _ = self.parser.parse_known_args(
+#             [
+#                 "-gpu",
+#                 "0",
+#                 "-data_type",
+#                 "text",
+#                 "-src_lang",
+#                 "en",
+#                 "-tgt_lang",
+#                 "fr",
+#                 "-model",
+#                 "/home/micheleb/models/scaleUpMNMT/opus12/opus12.50.adaf_step_3000_",
+#                 "-src",
+#                 "/home/micheleb/data/scaleUpMNMT/prepare_opus_data_out/supervised/en-fr/opus.en-fr-dev.en.sp",
+#                 "-output",
+#                 "/home/micheleb/projects/OpenNMT-py-v2/translate_test.tmp",
+#                 "-use_attention_bridge",
+#             ]
+#         )
+#         translate(opt)

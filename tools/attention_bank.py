65113a8ac69d1f817c4d110d920bf1b94375d334
Mickus Timothee <mickus@lx9-400-013.hum.helsinki.fi>
2022-09-20 12:47:49 +0300

    extracting banks

---
+++ tools/attention_bank.py
@@ -0,0 +1,166 @@
+import argparse
+import collections
+import itertools
+import pickle
+
+import numpy as np
+import torch
+
+from onmt.inputter import DynamicDataset, str2sortkey
+from onmt.inputters.text_dataset import InferenceDataIterator
+from onmt.model_builder import load_test_multitask_model
+from onmt.opts import build_bilingual_model, translate_opts, validate_translate_opts
+from onmt.utils.logging import init_logger
+from onmt.utils.parse import ArgumentParser
+
+
+def get_opts():
+    """parse command line options"""
+
+    parser = argparse.ArgumentParser()
+    translate_opts(parser, dynamic=True)
+    build_bilingual_model(parser)
+
+    subparsers = parser.add_subparsers(dest='command')
+
+    subparser = subparsers.add_parser('extract')
+    subparser.add_argument('--src_sentences', type=pathlib.Path)
+    subparser.add_argument('--dump_file', type=pathlib.Path)
+
+    subparser = subparsers.add_parser('estimate')
+    subparser.add_argument('--src_sentences', type=pathlib.Path, required=True)
+    subparser.add_argument('--param_save_file', type=pathlib.Path, required=True)
+    # subparser.add_argument('--do_generate', action='store_true')
+    # subparser.add_argument('--top_k', type=int, default=None)
+
+    subparser = subparsers.add_parser('classify')
+    subparser.add_argument('--train_sentences', type=pathlib.Path, nargs='+')
+    subparser.add_argument('--train_labels', type=pathlib.Path)
+    subparser.add_argument('--test_sentences', type=pathlib.Path, nargs='+')
+    subparser.add_argument('--test_labels', type=pathlib.Path)
+
+    opts = parser.parse_args()
+
+    return opts
+
+
+def _extract(sentences_file, model, fields, transforms):
+    """sub-routine to embed file"""
+    example_stream = InferenceDataIterator(sentences_file, None, None, transforms)
+    example_stream = DynamicDataset(fields, data=example_stream, sort_key=str2sortkey['text'])
+    for batch in example_stream:
+        src, src_lengths = batch.src
+        enc_states, memory_bank, src_lengths, mask = model.encoder[f"encoder{self.enc_id}"](
+            src, src_lengths
+        )
+        memory_bank, alphas = model.attention_bridge(memory_bank, mask)
+        yield (bank, src_lengths)
+
+
+def extract(opts, fields, model, model_opt, transforms):
+    """Compute representations drawn from the encoder and save them to file."""
+    sentence_reps = []
+    for src, src_length in _extract(opts.src_sentences, model, fields, transforms):
+        for sentence, length in zip(src.unbind(1), src_length):
+            sentence_reps.append(sentence[:length])
+    torch.save(sentence_reps, opts.dump_file)
+
+
+def estimate(opts, fields, model, model_opt, transforms):
+    """Estimate the matrix-variate distribution of representations drawn from the encoder."""
+    try:
+        import sklearn.covariance
+    except ImportError:
+        raise RuntimeError('please install scikit-learn')
+
+    assert model.attention_bridge.is_fixed_length, "Can't estimate matrix-variate distribution with varying shapes"
+    sentence_reps, _ = zip(*_extract(opts.src_sentences, model, fields, transforms))
+    sentence_reps = torch.cat(sentence_reps, dim=1).transpose(0, 1)
+    b, fixed_len, feats = sentence_reps.size()
+    sentence_reps = sentence_reps.view(b, fixed_len * feats).cpu().numpy()
+    cov_obj = sklearn.covariance.EmpiricalCovariance()
+    cov_obj.fit(sentence_reps)
+    loc = torch.from_numpy(cov_obj.location_)
+    cov_mat = torch.from_numpy(cov_obj.covariance_)
+    torch.save([loc, cov_mat, fixed_len, feats], opts.param_save_file)
+    # TODO: need to implement some way of using this in translate.py
+    # how to use:
+    # loc, cov_mat, fixed_len, feats = torch.load(opts.param_save_file)
+    # dist = torch.distributions.MultivariateNormal(loc, covariance_matrix=cov_mat,)
+    # def sampling_fn():
+    #     return dist.sample().view(fixed_len, 1, feats)
+    # return sampling_fn
+
+
+def classify(opts, fields, model, model_opt, transforms):
+    """Learn a simple SGD classifier using representations drawn from the encoder."""
+    try:
+        import sklearn.classification
+    except ImportError:
+        raise RuntimeError('please install scikit-learn')
+
+    label2idx = collections.defaultdict(itertools.count().__next__)
+
+    X_trains = []
+    for train_file in opts.train_sentences:
+        embedded = []
+        for src, src_lengths in zip(*_extract(train_file, model, fields, transforms)):
+            mask = torch.arange(src.size(0)) >= src_lengths
+            embedded.append(src.masked_fill(mask, 0.).sum(0))
+        X_trains.append(torch.cat(embedded, dim=0))
+    X_train = torch.cat(X_trains, dim=-1).numpy()
+
+    with open(opts.train_labels, 'r') as istr:
+        istr = map(str.strip, istr)
+        istr = map(label2idx.__getitem__, istr)
+        y_train = np.array(list(istr))
+
+    label2idx = dict(label2idx)
+
+    X_test = []
+    for test_file in opts.train_sentences:
+        embedded = []
+        for src, src_lengths in zip(*_extract(test_file, model, fields, transforms)):
+            mask = torch.arange(src.size(0)) >= src_lengths
+            embedded.append(src.masked_fill(mask, 0.).sum(0))
+        X_test.append(torch.cat(embedded, dim=0))
+    X_test = torch.cat(X_test, dim=-1).numpy()
+
+    with open(opts.test_labels, 'r') as istr:
+        istr = map(str.strip, istr)
+        istr = map(label2idx.__getitem__, istr)
+        y_test = np.array(list(istr))
+
+    model = sklearn.classification.SGDClassifier()
+    model.fit(X_train, y_train)
+    logger.log(f'Score: {model.score(X_test, y_test) * 100:.4f}%')
+
+
+@torch.no_grad()
+def main():
+    """Main entry point"""
+    opts = get_opts()
+    ArgumentParser._get_all_transform_translate(opt)
+    ArgumentParser._validate_transforms_opts(opt)
+    ArgumentParser.validate_translate_opts_dynamic(opt)
+    logger = init_logger(opt.log_file)
+
+    fields, model, model_opt = load_test_multitask_model(opt, opt.model_path)
+    command_fn = {
+        fn.__name__: fn for fn in
+        [extract, extimate, classify]
+    }[opts.command]
+
+    # Build transforms
+    transforms_cls = get_transforms_cls(opt._all_transform)
+    transforms = make_transforms(opt, transforms_cls, translator.fields)
+    data_transform = [
+        transforms[name] for name in opt.transforms if name in transforms
+    ]
+    transform = TransformPipe.build_from(data_transform)
+
+    command_fn(opts, fields, model, model_opt, transforms)
+
+
+if __name__ == '__main__':
+    main()

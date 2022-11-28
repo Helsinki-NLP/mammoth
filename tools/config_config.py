c9f495a1064f280e52293e1ecabcaabe9cee515b
Mickus Timothee <mickus@lx7-humtdk75.hum.helsinki.fi>
2022-11-28 17:13:34 +0200

    chaining command

--- tools/config_config.py
+++ tools/config_config.py
@@ -13,7 +13,7 @@ def load_yaml(fname):
     return config, fname
 
 
-def load_simmat_csv(fname):
+def load_distmat_csv(fname):
     with open(fname, 'r') as istr:
         reader = csv.reader(istr)
         header = next(reader)
@@ -34,27 +34,45 @@ def save_yaml(opts):
         yaml.dump(opts.in_config[0], ostr, default_flow_style=False, allow_unicode=True)
 
 
+def add_configs_args(parser):
+    parser.add_argument('--in_config', required=True, type=load_yaml)
+    parser.add_argument('--out_config', type=str)
+
+
+def add_corpora_schedule_args(parser):
+    parser.add_argument('--use_weights', action='store_true')
+
+
+def add_define_group_args(parser):
+    parser.add_argument('--distance_matrix', required=True, type=load_distmat_csv)
+    parser.add_argument('--cutoff_threshold', type=float)
+    parser.add_argument('--n_groups', type=int)
+
+
+def add_allocate_device_args(parser):
+    parser.add_argument('--n_devices', type=int, required=True)
+    parser.add_argument('--n_nodes', type=int, required=True)
+
+
 def get_opts():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest='command')
     parser_corpora_schedule = subparsers.add_subparser('corpora_schedule')
-    parser_corpora_schedule.add_argument('--in_config', required=True, type=load_yaml)
-    parser_corpora_schedule.add_argument('--out_config', type=str)
-    parser_corpora_schedule.add_argument('--use_weights', action='store_true')
+    add_configs_args(parser_corpora_schedule)
+    add_corpora_schedule_args(parser_corpora_schedule)
     parser_define_group = subparsers.add_subparser('define_group')
-    parser_define_group.add_argument('--in_config', required=True, type=load_yaml)
-    parser_define_group.add_argument('--out_config', type=str)
-    parser_define_group.add_argument('--similarity_matrix', required=True, type=load_simmat_csv)
-    parser_define_group.add_argument('--cutoff_threshold', type=float)
-    parser_define_group.add_argument('--n_groups', type=int)
+    add_configs_args(parser_define_group)
+    add_define_group_args(parser_define_group)
     parser_allocate_devices = subparsers.add_subparser('allocate_devices')
-    parser_allocate_devices.add_argument('--in_config', required=True, type=load_yaml)
-    parser_allocate_devices.add_argument('--out_config', type=str)
-    parser_allocate_devices.add_argument('--n_devices', type=int, required=True)
-    parser_allocate_devices.add_argument('--n_nodes', type=int, required=True)
+    add_configs_args(parser_allocate_devices)
+    add_allocate_device_args(parser_allocate_devices)
     parser_adapter_config = subparsers.add_subparser('adapter_config')
-    parser_adapter_config.add_argument('--in_config', required=True, type=load_yaml)
-    parser_adapter_config.add_argument('--out_config', type=str)
+    add_configs_args(parser_adapter_config)
+    parser_config_all = subparsers.add_subparser('config_all')
+    add_configs_args(parser_config_all)
+    add_corpora_schedule_args(parser_config_all)
+    add_define_group_args(parser_config_all)
+    add_allocate_device_args(parser_config_all)
     return parser.parse_args()
 
 
@@ -76,35 +94,32 @@ def corpora_schedule(opts):
         total_steps = opts.in_config[0].get('train_steps', 100_000)
         for cname, corpus in opts.in_config[0]['data'].items():
             corpus['introduce_at_training_step'] = round(total_steps * corpora_weights[cname])
-    save_yaml(opts)
 
 
 def define_group(opts):
-    sim_langs = set(opts.similarity_matrix['header'])
+    sim_langs = set(opts.distance_matrix['header'])
     corpus_langs = set()
     for cname, corpus in opts.in_config[0]['data'].items():
         assert all([(lng in sim_langs) for lng in corpus['src_tgt'].split('-')]), \
             f'corpus {cname}: one language (either {" or ".join(corpus["src_tgt"].split("-"))} ' \
-            f'was not found in the similarity matrix (supports {" ".join(sim_langs)})'
+            f'was not found in the distance matrix (supports {" ".join(sim_langs)})'
         corpus_langs = corpus_langs | set(corpus['src_tgt'].split('-'))
     if sim_langs != corpus_langs:
         warnings.warn(
-            f"languages in the similarity matrix are unused ({', ' .join(sim_langs - corpus_langs)})"
+            f"languages in the distance matrix are unused ({', ' .join(sim_langs - corpus_langs)})"
         )
 
     group_idx = AgglomerativeClustering(
         n_clusters=opts.n_groups,
         affinity='precomputed',
         distance_threshold=opts.cutoff_threshold,
-    ).fit_predict(opts.similarity_matrix['data']).tolist()
-    groups = {lang: f'group{idx}' for lang, idx in zip(opts.similarity_matrix['header'], group_idx)}
+    ).fit_predict(opts.distance_matrix['data']).tolist()
+    groups = {lang: f'group{idx}' for lang, idx in zip(opts.distance_matrix['header'], group_idx)}
 
     for cname, corpus in opts.in_config[0]['data'].items():
         src, tgt = corpus['src_tgt'].split('-')
         corpus['enc_sharing_group'] = [groups[src], 'full']
         corpus['dec_sharing_group'] = [groups[tgt], 'full', groups[tgt]]
-
-    save_yaml(opts)
 
 
 def allocate_devices(opts):
@@ -115,12 +130,20 @@ def adapter_config(opts):
     pass
 
 
+def config_all(opts):
+    corpora_schedule(opts)
+    define_group(opts)
+    allocate_devices(opts)
+    adapter_config(opts)
+
+
 if __name__ == '__main__':
     opts = get_opts()
     if not opts.out_config:
         opts.out_config = opts.in_config[1]
     main = {
         func.__name__: func
-        for func in (corpora_schedule, define_group, allocate_devices, adapter_config)
+        for func in (corpora_schedule, define_group, allocate_devices, adapter_config, config_all)
     }[opts.command]
     main(opts)
+    save_yaml(opts)

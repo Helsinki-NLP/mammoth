7bfdfd015a2ae7d25a45a6f404b6a0e8a58899ca
Stig-Arne Grönroos <stig.gronroos@gmail.com>
2023-02-13 16:51:19 +0200

    Revert "Lots of qui-logging"
    This reverts commit 79e48de8b2a47beafca21fa0241ab762a869f3da.

--- onmt/utils/distributed.py
+++ onmt/utils/distributed.py
@@ -194,49 +194,34 @@ def only_ready_reduce_and_rescale_grads(named_parameters, group=None):
 
     # Communicate the ready bits, and reduce them using summation.
     # This gives the number of non-dummy gradients participating, for normalization
-    logger.warning(f'DEBUG: start of Communicate the ready bits. ready_list = {ready_list}')
     ready_t = torch.tensor(ready_list).to(device)
     if group is None:
-        logger.warning('DEBUG: no group, before')
         torch.distributed.all_reduce(ready_t)
-        logger.warning('DEBUG: no group, after')
     else:
-        logger.warning('DEBUG: with group, before')
         torch.distributed.all_reduce(ready_t, group=group)
-        logger.warning('DEBUG: with group, after')
     rescale_denoms = ready_t  # after reduction
-    logger.warning(f'DEBUG: after reduction. rescale_denoms = {rescale_denoms}')
 
     # Omit if all nodes sent a zero ready bit
     grads = [p.grad.data for name, p in require_grad]
-    logger.warning(f'DEBUG: grads len A {len(grads)}')
     grads = [grad for (grad, denom) in zip(grads, rescale_denoms) if denom > 0]
-    logger.warning(f'DEBUG: grads len B {len(grads)}')
     rescale_denoms = [denom for denom in rescale_denoms if denom > 0]
-    logger.warning(f'DEBUG: rescale_denoms len B {len(rescale_denoms)}')
     assert len(grads) == len(rescale_denoms)
-    logger.warning('DEBUG: passed assert')
     if len(grads) == 0:
-        logger.warning('DEBUG: returning early')
         return
-    logger.warning('DEBUG: NOT returning early')
 
     # If not, then set has_grad also on devices that did not train the parameter themselves.
     # They now have a grad that they received from the other devices.
     for name, p in require_grad:
         p.has_grad = True
-    logger.warning('DEBUG: everything now has has_grad')
 
     # All devices communicate either a real gradient or a dummy zeros of the same size
     # Can not use rescale_denom, as each grad may have its own denominator
     all_reduce_and_rescale_tensors(grads, rescale_denom=1, group=group)
-    logger.warning(f'DEBUG: after allreduce. grads len {len(grads)}')
 
     # Normalize using the previously computed values
     for grad, denom in zip(grads, rescale_denoms):
         if denom > 0:
             grad.div_(denom)
-    logger.warning('DEBUG: after allreduce, end of function')
     # Note: p.has_grad is reused in the optimizer to prevent the untrained components from being stepped
 
 
@@ -623,27 +608,17 @@ class TaskQueueManager:
     def from_opt(cls, opt: Namespace, world_context: WorldContext):
         n_tasks = len(opt.data)
 
-        # Sorting the keys, to ensure that tasks have a consistent order across devices.
-        # This in turn ensures the order in which components are created from those tasks.
-        corpus_ids = sorted(opt.data.keys())
-
         if world_context.is_distributed():
             if any(task.get('node_gpu', None) is not None for task in opt.data.values()):
-                node_gpu = [
-                    tuple(int(y) for y in opt.data[corpus_id]['node_gpu'].split(':', 1))
-                    for corpus_id in corpus_ids]
+                node_gpu = [tuple(int(y) for y in task['node_gpu'].split(':', 1)) for task in opt.data.values()]
             else:
                 # When --node_gpu is not set, assume an assigment that fills gpus in rank order
                 node_gpu = cls._default_node_gpu(n_tasks, world_context.n_nodes, world_context.gpus_per_node)
         else:
             node_gpu = [(0, 0)] * n_tasks
 
-        enc_sharing_group = [
-            opt.data[corpus_id].get('enc_sharing_group', None) for corpus_id in corpus_ids
-        ]
-        dec_sharing_group = [
-            opt.data[corpus_id].get('dec_sharing_group', None) for corpus_id in corpus_ids
-        ]
+        enc_sharing_group = [task.get('enc_sharing_group', None) for task in opt.data.values()]
+        dec_sharing_group = [task.get('dec_sharing_group', None) for task in opt.data.values()]
         if any(x is not None for x in enc_sharing_group):
             assert all(len(enc_ids) == len(opt.enc_layers) for enc_ids in enc_sharing_group)
         else:
@@ -658,6 +633,8 @@ class TaskQueueManager:
             # it is assumed that there is only one decoder stack and it is language specific
             if not len(opt.dec_layers) == 1:
                 raise Exception('With more than one decoder stack, you must explictly define dec_sharing_group')
+
+        corpus_ids = opt.data.keys()
 
         tasks = []
         uses_adapters = False

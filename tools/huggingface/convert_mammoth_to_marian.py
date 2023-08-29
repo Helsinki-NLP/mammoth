ae7e070ac4628090b087a93b1746a862cb62118b
Maksim Tikhonov <maksimt@lingea.cz>
2023-08-29 15:24:59 +0300

    add sum for enc/dec layers

--- tools/huggingface/convert_mammoth_to_marian.py
+++ tools/huggingface/convert_mammoth_to_marian.py
@@ -9,11 +9,9 @@ from onmt.opts import train_opts
 def _get_parser():
     parser = ArgumentParser(description='convert_mammoth_to_marian.py')
     train_opts(parser)
-    parser.add_argument('hf_ckpt_dir', required=True)
+    parser.add_argument('--hf_ckpt_dir', required=True)
+    parser.add_argument('--out_dir', required=True)
     return parser
-
-
-
 
 
 def convert_mammoth_to_marian(mammoth_config_path: str, marian_config_path: str) -> None:
@@ -28,10 +26,10 @@ def convert_mammoth_to_marian(mammoth_config_path: str, marian_config_path: str)
             vocab_size=config_dict["src_vocab_size"],
             decoder_vocab_size=config_dict["src_vocab_size"],
             max_position_embeddings=1024,  # default
-            encoder_layers=config_dict["enc_layers"][0],
+            encoder_layers=sum(config_dict["enc_layers"]),
             encoder_ffn_dim=config_dict["transformer_ff"],
             encoder_attention_heads=config_dict["heads"],
-            decoder_layers=config_dict["dec_layers"][0],
+            decoder_layers=sum(config_dict["dec_layers"]),
             decoder_ffn_dim=config_dict["transformer_ff"],
             decoder_attention_heads=config_dict["heads"],
             encoder_layerdrop=0.0,  # default
@@ -40,7 +38,7 @@ def convert_mammoth_to_marian(mammoth_config_path: str, marian_config_path: str)
             is_encoder_decoder=True,  # default
             activation_function=config_dict['pos_ffn_activation_fn'],  # default
             d_model=config_dict['rnn_size'],  # default
-            dropout=confiog_dict['dropout'],  # default
+            dropout=config_dict['dropout'],  # default
             attention_dropout=0,  # default
             activation_dropout=0,  # default
             init_std=0.02,  # default
@@ -50,15 +48,9 @@ def convert_mammoth_to_marian(mammoth_config_path: str, marian_config_path: str)
             eos_token_id=0,  # default
             forced_eos_token_id=0,  # default
             share_encoder_decoder_embeddings=True,  # default
-        )
+    )
 
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--src", "-s", dest="mammoth_config_path", default=sys.stdin)
-    return parser.parse_args()
-
-
-if __name__ == "__main__":
-    args = parse_args()
-    convert_mammoth_to_marian(**args.__dict__)
+# if __name__ == "__main__":
+#     args = parse_args()
+#     convert_mammoth_to_marian(**args.__dict__)

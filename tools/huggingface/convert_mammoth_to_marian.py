4b99a5e6d1dbdfb2b6e3766ba20bd5e7e6c53f78
Maksim Tikhonov <maksimt@lingea.cz>
2023-08-29 11:38:10 +0300

    add marian config initialization

--- tools/huggingface/convert_mammoth_to_marian.py
+++ tools/huggingface/convert_mammoth_to_marian.py
@@ -1,5 +1,6 @@
 from transformers import MarianConfig
 import sys
+import yaml
 import argparse
 
 
@@ -10,7 +11,35 @@ def convert_mammoth_to_marian(mammoth_config_path: str, marian_config_path: str)
         open(mammoth_config_path, "r") if mammoth_config_path is not sys.stdin else sys.stdin as mammoth_config,
         open(marian_config_path, "w") if marian_config_path is not sys.stdout else sys.stdout as marian_config
     ):
-        pass
+        config_dict = yaml.safe_load(mammoth_config)
+        marian = MarianConfig(
+            vocab_size=config_dict["src_vocab_size"],
+            decoder_vocab_size=config_dict["src_vocab_size"],
+            max_position_embeddings=1024,  # default
+            encoder_layers=config_dict["enc_layers"][0],
+            encoder_ffn_dim=config_dict["transformer_ff"],
+            encoder_attention_heads=config_dict["heads"],
+            decoder_layers=config_dict["dec_layers"][0],
+            decoder_ffn_dim=config_dict["transformer_ff"],
+            decoder_attention_heads=config_dict["heads"],
+            encoder_layerdrop=0.0,  # default
+            decoder_layerdrop=0.0,  # default
+            use_cache=True,  # default
+            is_encoder_decoder=True,  # default
+            activation_function="gelu",  # default
+            d_model=1024,  # default
+            dropout=0.1,  # default
+            attention_dropout=0,  # default
+            activation_dropout=0,  # default
+            init_std=0.02,  # default
+            decoder_start_token_id=58100,  # default
+            scale_embedding=False,  # default
+            pad_token_id=58100,  # default
+            eos_token_id=0,  # default
+            forced_eos_token_id=0,  # default
+            share_encoder_decoder_embeddings=True,  # default
+        )
+
 
 def parse_args():
     parser = argparse.ArgumentParser()

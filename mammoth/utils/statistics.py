7229141098fa65e4459d69a5734081b3ef1ab8ed
Stig-Arne Grönroos <stig.gronroos@gmail.com>
2024-10-07 14:29:13 +0300

    Number of correct defaults to None, but can handle zero

--- mammoth/utils/statistics.py
+++ mammoth/utils/statistics.py
@@ -20,7 +20,7 @@ class Statistics(object):
     * elapsed time
     """
 
-    def __init__(self, loss=0, n_words=0, n_correct=0):
+    def __init__(self, loss=0, n_words=0, n_correct=None):
         self.loss = loss
         self.n_words = n_words
         self.n_correct = n_correct

1490108d836ba52a1a1af951aa1512509d8a6461
Stig-Arne Grönroos <stig.gronroos@gmail.com>
2024-11-04 10:30:45 +0200

    Remove old transformer decoder.
    The old transformer encoder classes are still needed for the obsolete
    attention bridge. Removal is pending.

--- mammoth/modules/__init__.py
+++ mammoth/modules/__init__.py
@@ -8,13 +8,8 @@ from mammoth.modules.encoder import EncoderBase
 from mammoth.modules.transformer_encoder import TransformerEncoder
 from mammoth.modules.mean_encoder import MeanEncoder
 
-from mammoth.modules.decoder import DecoderBase
-from mammoth.modules.transformer_decoder import TransformerDecoder
-
 
 __all__ = [
-    "DecoderBase",
-    "TransformerDecoder",
     "EncoderBase",
     "TransformerEncoder",
     "MeanEncoder",

1e6b03a6ce3853131d093d73bf91fdad1ed2a48d
Stig-Arne Grönroos <stig.gronroos@gmail.com>
2024-12-16 10:25:20 +0200

    Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description='Massively Multilingual Modular Open Translation @ Helsinki',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='0.1.1',
+    version='0.2.1',
     packages=find_packages(),
     project_urls={
         "Documentation": "https://helsinki-nlp.github.io/mammoth/",

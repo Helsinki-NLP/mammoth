3e147d4fb771eca39e049fb6e33a1fc3bbde5293
Anssi Yli-Jyra <{ID}+{username}@users.noreply.github.com>
2025-09-11 21:08:37 +0300

    updated build-venv-mammoth-hf.sh

--- helper/bin/conf/build-venv-mammoth-hf.sh
+++ helper/bin/conf/build-venv-mammoth-hf.sh
@@ -1,43 +1,53 @@
 #! /usr/bin/bash
 # (c) 2025 Anssi Yli-Jyrä, CC-BY
 
-# This script assumes $ACCOUNT, $PROJHOME
-# The script pulls updates in $HOME/git/mammoth
-# The script builds $HOME/git/mammoth-hf
-# The script creates symlink $PROJHOME/mammoth-hf
-# The script deletes/rebuilds $PROJHOME/venv/mammoth-hf
-
-: "${ACCOUNT:?❌ ACCOUNT is not set}"           # project_462000964
-: "${PROJHOME:?❌ PROJHOME is not set}"         # /project/$ACCOUNT/members/$USER
-source $PROJHOME/bin/slurm/module-loads.sh
-
-# clone or pull current mammoth at user's home git
+# The script pulls updates in $GITHOME/mammoth
+# The script checkouts feat/hf_integration as $GITHOME/mammoth-hf
+# The script checkouts feat/helper         as $GITHOME/mammoth-helper
 : "${GITHOME:?❌ GITHOME is not set}"           # $HOME/git
 cd $GITHOME/mammoth
-git pull
-# git clone https://github.com/Helsinki-NLP/mammoth.git mammoth
+git pull   # get the latest updates
+git worktree add ../mammoth-hf     feat/hf_integration
+git worktree add ../mammoth-helper feat/helper
 
+# This script assumes $PROJHOME
+# The script creates symlink $PROJHOME/mammoth-hf
+# The script deletes/rebuilds $PROJHOME/venv/mammoth-hf
+: "${PROJHOME:?❌ PROJHOME is not set, suggestion: export PROJHOME=/project/$ACCOUNT/members/$USER}"
 cd $PROJHOME
-export MAMMOTH_REPO=$PROJHOME/mammoth
-: "${MAMMOTH_REPO:?❌ MAMMOTH_REPO is not set}"
-ln -s $HOME/git/mammoth $MAMMOTH_REPO
+ln -s $GITHOME/mammot-hf            mammoth
+ln -s $GITHOME/mammot-helper/helper .
+ln -s helper/*                      .
 
-export MAMMOTH_HF_BASE=$PROJHOME/mammoth-hf
-cd $MAMMOTH_REPO
-git worktree add ../mammoth-hf feat/hf_integration
-ln -s $HOME/git/mammoth-hf $MAMMOTH_HF_BASE
-: "${MAMMOTH_HF_BASE:?❌ MAMMOTH_HF_BASE is not set}"
+# Load the partition/L software stack since we are running on login node
+# If this script is run inside srun, partion/C or partition/G will be loaded
+cd $PROJHOME
+export NODE_KIND=gpu  # The partition/G software stack is forced even on login node
+source helper/bin/slurm/module-loads.sh   
+# After this, python is a wrapper that launches a pytorch/rocm container and runs python.
 
-export MAMMOTH_VENV=$PROJHOME/venv/mammoth-hf
-: "${MAMMOTH_VENV:?❌ MAMMOTH_VENV is not set}"
-rm -Rf $PROJHOME/venv/mammoth-hf
-python -m venv --system-site-packages "$MAMMOTH_VENV"
-source $MAMMOTH_VENV/bin/activate
+# Temporary hack: I do not want to use setup.py provided by feat/hf_integration
+# but rather setup-mammoth-hf.py provided in helper/bin/conf.  Rename and copy:
+cd $PROJHOME/mammoth
+mv setup.py setup.py.old
+cp $PROJHOME/helper/bin/conf/setup-mammoth-hf.py setup.py
+
+# Build virtual environment for this particular branch (feat/hf_integration as mammoth-hf)
+# We use `pip -e .` to install mammoth to the virtual environment via symlinks,
+# i.e., in editable form.  This uses the branch-specific `setup.py` file 
+cd $PROJHOME
+rm -Rf venv/mammoth-hf
+# Python/pip will run now in a modulerized-container with GPU-enabled pytorch and rocm libraries
+# There is no need to do installation inside `srun` nor singularity, but partition/G is useful.
+# We create virtual environment so that it inherits pytorch etc in the container
+python -m venv --system-site-packages venv/mammoth-hf
+source venv/mammoth-hf/bin/activate
 python -m pip install --upgrade pip
-# pip install -r $MAMMOTH_HF_BASE/requirements.txt
-pip install -r $PROJHOME/bin/conf/requirements-lumi.txt
+cd $PROJHOME/mammoth
+pip install -e . 
+deactivate
 
-# expect something like this on LUMI:
+# Expect something like this on LUMI:
 
 # ERROR: pip's dependency resolver does not currently take into account all the packages that are installed.
 #        This behaviour is the source of the following dependency conflicts.
@@ -49,4 +59,3 @@ pip install -r $PROJHOME/bin/conf/requirements-lumi.txt
 # Successfully installed ConfigArgParse-1.7.1 MarkupSafe-3.0.2 certifi-2025.8.3 charset-normalizer-3.4.3 einx-0.3.0 frozendict-2.4.6 fsspec-2025.7.0 idna-3.10 loguru-0.7.3 networkx-3.5 packaging-25.0 protobuf-6.32.0 setuptools-80.9.0 sympy-1.14.0 tensorboard-2.20.0 transformers-4.55.4 typing_extensions-4.14.1 urllib3-2.5.0
 
 
-deactivate

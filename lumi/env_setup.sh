9673ce6551308d7f1245dd264676d695685d3acf
chaowang0524 <chaowang0524@outlook.com>
2025-09-15 20:10:28 +0300

    updated the path to requirements_lumi.txt

--- lumi/env_setup.sh
+++ lumi/env_setup.sh
@@ -12,7 +12,7 @@ singularity exec \
 singularity exec \
     -B $your_path_in_lumi:$your_path_in_lumi:rw \
     /appl/local/containers/sif-images/lumi-pytorch-rocm-6.2.4-python-3.12-pytorch-v2.7.1.sif \
-    $your_path_in_lumi/.venv/bin/pip install -r $path_to_mammoth/requirements_lumi.txt
+    $your_path_in_lumi/.venv/bin/pip install -r $path_to_mammoth/lumi/requirements_lumi.txt
 
 
 # Example usage:
@@ -27,4 +27,4 @@ singularity exec \
 # singularity exec \
 #     -B /scratch/project_462000964/members/wangchao:/scratch/project_462000964/members/wangchao:rw \
 #     /appl/local/containers/sif-images/lumi-pytorch-rocm-6.2.4-python-3.12-pytorch-v2.7.1.sif \
-#     /scratch/project_462000964/members/wangchao/.venv/bin/pip install -r /scratch/project_462000964/members/wangchao/mammoth/requirements_lumi.txt
+#     /scratch/project_462000964/members/wangchao/.venv/bin/pip install -r /scratch/project_462000964/members/wangchao/mammoth/lumi/requirements_lumi.txt
